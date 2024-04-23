b380ae0ed9000cd74e4a227cab0d62a32e11fa96
Melvin Bubini <melvin.bubini@gmail.com>
2024-04-23 11:49:56 +0200

    andra utkast för klar. Delete och post tillagda

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/cv").then(() => {
 // job schema 
 const jobSchema = new mongoose.Schema({
     companyname: {
-        type: String, 
+        type: String,
         required: true,
     },
     jobtitle: {
@@ -36,7 +36,7 @@ const job = mongoose.model("Job", jobSchema);
 
 // routes
 app.get("/api", (req, res) => {
-    res.json({message: "Welcome to my api"});
+    res.json({ message: "Welcome to my api" });
 });
 
 app.get("/jobs", async (req, res) => {
@@ -50,12 +50,43 @@ app.get("/jobs", async (req, res) => {
 });
 
 app.post("/jobs", async (req, res) => {
+    let companyname = req.body.companyname;
+    let jobtitle = req.body.jobtitle;
+    let location = req.body.location;
+
     try {
-        let result = await job.create(req.body);
+        if (!companyname || !jobtitle || !location) {
+            throw new Error("companyname, jobtitle, or location not included");
+        }
+
+        let newJob = new job({
+            companyname: companyname,
+            jobtitle: jobtitle,
+            location: location
+        });
+
+        let result = await newJob.save();
 
         res.json(result);
     } catch (error) {
-        return res.status(400).json(error);
+        return res.status(400).json({error: error.message});
+    }
+});
+
+// delete job
+app.delete("/jobs/:id", async (req, res) => {
+    let id = req.params.id;
+
+    try {
+        let result = await job.deleteOne({ _id: id });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ message: "No job found" });
+        }
+
+        return res.json({ message: "Job deleted with id: " + id });
+    } catch (error) {
+        return res.status(500).json({ error: "Something went wrong: " + error });
     }
 });
 
